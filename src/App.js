8ff6c493cb97556a9958eda673c926718eb31ffc
sh0la <sholaadeniyi@gmail.com>
2020-01-14 23:10:09 -0600

    Use filter to modify array in state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
 import Table from './Table';
 
+removeCharacter = index => {
+  const { characters } = this.state
+
+
+  this.setState({
+    characters: characters.filter((character, i) => {
+      return i !== index
+    }),
+  })
+}
+
+class App extends Component {
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job:  'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspiring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ],
+  }
+
+}
+
+
 class App extends Component {
   render() {
+    const characters = [
+      
+    ]
+
     return (
       <div className="container">
-        <Table />
+        <Table characterData={characters} />
       </div>
     )
   }
